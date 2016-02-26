3ac053f066621ee51b44a2f46ec0360c358caf59
Paolo Romolini <p.romolini@nephila.it>
2016-02-26 17:48:25 +0100

    Add middleware

---
+++ djangocms_redirect/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django.contrib import admin
+from django.contrib.redirects.models import Redirect
+
+
+@admin.register(Redirect)
+class RedirectAdmin(admin.ModelAdmin):
+    list_display = ('old_path', 'new_path', 'response_code')
+    list_filter = ('site',)
+    search_fields = ('old_path', 'new_path')
+    radio_fields = {'site': admin.VERTICAL}
---
+++ djangocms_redirect/middleware.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django import http
+from django.apps import apps
+from django.conf import settings
+from django.contrib.redirects.models import Redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ImproperlyConfigured
+
+
+class RedirectFallbackMiddleware(object):
+
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = http.HttpResponseGone
+    response_redirect_class = http.HttpResponsePermanentRedirect
+
+    def __init__(self):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                "You cannot use RedirectFallbackMiddleware when "
+                "django.contrib.sites is not installed."
+            )
+
+    def process_request(self, request):
+
+        full_path = request.get_full_path()
+        current_site = get_current_site(request)
+
+        r = None
+        try:
+            r = Redirect.objects.get(site=current_site, old_path=full_path)
+        except Redirect.DoesNotExist:
+            pass
+        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
+            try:
+                r = Redirect.objects.get(
+                    site=current_site,
+                    old_path=request.get_full_path(force_append_slash=True),
+                )
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == '':
+                return self.response_gone_class()
+            return self.response_redirect_class(r.new_path)
+
+        # No redirect was found. Return the response.
+        return response
