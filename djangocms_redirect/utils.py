79958ad5dc3b47a66a7889807d3a5cfabf653f7a
Iacopo Spalletti <i.spalletti@nephila.digital>
2019-12-25 20:01:53 +0100

    Minor cache key generation logic and docs refactoring (#28)

--- djangocms_redirect/utils.py
+++ djangocms_redirect/utils.py
@@ -3,10 +3,13 @@ import hashlib
 
 
 def get_key_from_path_and_site(path, site_id):
-    '''
-    cache key has to be < 250 chars to avoid memcache.Client.MemcachedKeyLengthError
-    The best algoritm is SHA-224 whose output (224 chars) respects this limitations
-    '''
-    key = '{}_{}'.format(path, site_id)
-    key = hashlib.sha224(key.encode('utf-8')).hexdigest()
+    """
+    cache key has to be < 250 chars to avoid memcache.Client.MemcachedKeyLengthError.
+
+    SHA-224 is the best algorithm whose output (224 chars) respects this limitations:
+
+    total key length: Prefix (11) + HASH (224) + ID (max 3) + 2 separators (2) = 240
+    """
+    hashed_path = hashlib.sha224(path.encode('utf-8')).hexdigest()
+    key = 'CMSREDIRECT:{}:{}'.format(hashed_path, site_id)
     return key
